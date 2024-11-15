3b15de996efd771d4e3c1b472d00ee141d1fe8b6
ghofrane333 <ghofrane.boughalmi@gmail.com>
2024-11-15 13:45:02 +0100

    Update dashh.py

--- Desktop/dash/dashh.py
+++ Desktop/dash/dashh.py
@@ -188,4 +188,4 @@ def main():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))  # Changer ici pour 8000
-    app.run(host='0.0.0.0', port=port)
+    dash.run(host='0.0.0.0', port=port)
